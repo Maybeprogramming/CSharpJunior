283543bb3c32ec07b0145c009d19b152dfc1f027
Maybeonce <a-u-rom@yandex.ru>
2024-10-02 10:54:48 +0300

    Решение задачи 3

--- 03_Task/Program.cs
+++ 03_Task/Program.cs
@@ -4,6 +4,28 @@
     {
         static void Main()
         {
+            Console.Title = "ДЗ: Работа со строками";
+            string name;
+            string zodiacSign;
+            string age;
+            string workPlace;
+
+            Console.WriteLine("Как ваше имя?");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Сколько вам лет?");
+            age = Console.ReadLine();
+
+            Console.WriteLine("Какой ваш знак задиака?");
+            zodiacSign = Console.ReadLine();
+
+            Console.WriteLine("Где вы работаете?");
+            workPlace = Console.ReadLine();
+
+            Console.WriteLine(new string('-', 20));
+            Console.WriteLine($"Вас зовут {name}, вам {age}, вы {zodiacSign} и работаете на {workPlace}.");
+
+            Console.ReadKey();
         }
     }
 }
