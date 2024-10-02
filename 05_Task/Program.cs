96c52ff63a5200d5342293712e169e3932e84447
Maybeonce <a-u-rom@yandex.ru>
2024-10-02 14:10:03 +0300

    Решение задачи 5

--- 05_Task/Program.cs
+++ 05_Task/Program.cs
@@ -4,6 +4,23 @@
     {
         static void Main()
         {
+            Console.Title = "ДЗ: Перестановка местами значений";
+            string name = "Петров";
+            string lastname = "Иван";
+            string bufferString;
+
+            Console.WriteLine($"Данные до перестановки:");
+            Console.WriteLine($"Имя: {name}, Фамилия: {lastname}.");
+
+            bufferString = name;
+            name = lastname;
+            lastname = bufferString;
+
+            Console.WriteLine(new string('-', 30));
+            Console.WriteLine($"Данные после перестановки:");
+            Console.WriteLine($"Имя: {name}, Фамилия: {lastname}.");
+
+            Console.ReadKey();
         }
     }
 }
