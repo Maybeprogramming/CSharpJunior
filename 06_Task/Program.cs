c0de805a13f41bc859e7b60d49d716ff46453c24
Maybeonce <a-u-rom@yandex.ru>
2024-10-02 14:31:27 +0300

    Решение задачи 6

--- 06_Task/Program.cs
+++ 06_Task/Program.cs
@@ -4,6 +4,29 @@
     {
         static void Main()
         {
+            Console.Title = "ДЗ: Магазин кристаллов";
+
+            int coinsCount;
+            int cristalsCount = 0;
+            int priceOneCristal = 15;
+            int cristalsToBuy;
+
+            Console.WriteLine("Скольку золота у вас в кошельке?");
+            coinsCount = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Цена одного кристала равна: {priceOneCristal}");
+            Console.WriteLine("Сколько кристалов желаете купить?");
+
+            cristalsToBuy = Convert.ToInt32(Console.ReadLine());
+            cristalsCount += cristalsToBuy;
+            coinsCount -= cristalsToBuy * priceOneCristal;
+
+            Console.WriteLine($"Вы купили {cristalsToBuy} кристолов!");
+
+            Console.WriteLine(new string('-', 30));
+            Console.WriteLine($"Теперь у вас в кошельке:\n" +
+                              $"Золота: {coinsCount}\n" +
+                              $"Кристалов: {cristalsCount}");
         }
     }
 }
