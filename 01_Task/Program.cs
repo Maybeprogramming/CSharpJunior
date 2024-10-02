64163ec906f2cd00295aa7794dce4e8218cb0a2d
Maybeonce <a-u-rom@yandex.ru>
2024-10-02 09:39:36 +0300

    Решение задачи 1

--- 01_Task/Program.cs
+++ 01_Task/Program.cs
@@ -4,6 +4,16 @@
     {
         static void Main()
         {
+            int weaponSlot = 3;
+            float currentSpeed = 30.0f;
+            string personName = "Алексей";
+            bool isAlive = true;
+            double money = 10000.459;
+            char keySymbol = 'A';
+            uint health = 20;
+            long maxForce = 1234567890123456789;
+            short resourceNumber = 9;
+            byte dayOnMounth = 28;
         }
     }
 }
