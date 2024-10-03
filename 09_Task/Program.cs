a9eaa70c20a799e9dd64fc1892fa5eeef2703460
Maybeonce <a-u-rom@yandex.ru>
2024-10-03 10:04:56 +0300

    Работа над ошибками

--- 09_Task/Program.cs
+++ 09_Task/Program.cs
@@ -4,27 +4,17 @@
     {
         private static void Main()
         {            
-            const string CommandGoToCafe = "1";
-            const string CommandGoToCinema = "2";
-            const string CommandGoToBank = "3";
             const string CommandGoToExit = "exit";
 
             Console.Title = "ДЗ: Контроль выхода";
             string userInputCommand;
             bool isTired = false;
-            string WelcomeMessage = "Добро пожаловать в развлекательный центр";
-            string MenuHeaderMessage = "Введите команду для перехода";
-            string ContinueMessage = "\nЧтобы продолжить нажмите любую клавишу...";
+            string continueMessage = "\nЧтобы продолжить нажмите любую клавишу...";
 
             while (isTired == false)
             {
                 Console.Clear();
 
-                Console.WriteLine(WelcomeMessage);
-                Console.WriteLine(MenuHeaderMessage);
-                Console.WriteLine($"{CommandGoToCafe} - пройти в кафе");
-                Console.WriteLine($"{CommandGoToCinema} - пройти в кинотеатр");
-                Console.WriteLine($"{CommandGoToBank} - пройти к банкомату");
                 Console.WriteLine($"{CommandGoToExit} - выйти");
                 Console.Write("\nВаша команда: ");
 
@@ -34,19 +24,7 @@
                 {
                     case CommandGoToExit:
                         isTired = true;
-                        Console.WriteLine("Вы вышли из центра");
-                        break;
-
-                    case CommandGoToCafe:
-                        Console.WriteLine("Вы заказали кофе с круасаном в кафе");
-                        break;
-
-                    case CommandGoToCinema:
-                        Console.WriteLine("Вы купили билет на фильм: Дедпул и Росамаха");
-                        break;
-
-                    case CommandGoToBank:
-                        Console.WriteLine("Вы воспользовались банкоматом для снятия наличных");
+                        Console.WriteLine("Вы вышли...");
                         break;
 
                     default:
@@ -55,7 +33,7 @@
                         continue;
                 }
 
-                Console.WriteLine(ContinueMessage);
+                Console.WriteLine(continueMessage);
                 Console.ReadKey();
             }
         }
