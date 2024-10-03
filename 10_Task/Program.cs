24dc6c57efcf15a0e5d6f69c4e9f32721a3cd935
Maybeonce <a-u-rom@yandex.ru>
2024-10-03 09:43:05 +0300

    Решение задачи 10

--- 10_Task/Program.cs
+++ 10_Task/Program.cs
@@ -2,8 +2,21 @@
 {
     public class Program
     {
-        static void Main()
+        private static void Main()
         {
+            Console.Title = "ДЗ: Последовательность";
+            int startNumber = 5;
+            int endNumber = 103;
+            int stepNumber = 7;
+
+            for (int i = startNumber; i <= endNumber; i += stepNumber)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.ReadKey();
         }
     }
 }
+
+//Ответ: данный цикл For выбрал потому что известно начало, конец и шаг в цикле
