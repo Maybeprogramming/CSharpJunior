0ba0068a4a1ab2df65c3272a1ace2c790c039e78
Maybeonce <a-u-rom@yandex.ru>
2024-10-03 09:06:20 +0300

    Решение задачи 8

--- 08_Task/Program.cs
+++ 08_Task/Program.cs
@@ -4,6 +4,22 @@
     {
         static void Main()
         {
+            Console.Title = "ДЗ: Освоение циклов";
+            string userText;
+            int amountPrint;
+
+            Console.WriteLine("Введите сообщение:");
+            userText = Console.ReadLine();
+
+            Console.Write("Сколько раз хотите вывести? ");
+            amountPrint = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < amountPrint; i++)
+            {
+                Console.WriteLine(userText);
+            }
+
+            Console.ReadKey();
         }
     }
 }
