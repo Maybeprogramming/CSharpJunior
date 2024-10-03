731d76128a89cf64233cdfcec7989d32e044bfee
Maybeonce <a-u-rom@yandex.ru>
2024-10-03 09:27:58 +0300

    Решение задачи 9

--- 09_Task/Program.cs
+++ 09_Task/Program.cs
@@ -2,8 +2,61 @@
 {
     public class Program
     {
-        static void Main()
+        private static void Main()
         {
+            const string CommandGoToCafe = "1";
+            const string CommandGoToCinema = "2";
+            const string CommandGoToBank = "3";
+            const string CommandGoToExit = "exit";
+            const string WelcomeMessage = "Добро пожаловать в развлекательный центр ";
+            const string MenuHeaderMessage = "Введите команду для перехода";
+            const string ContinueMessage = "\nЧтобы продолжить нажмите любую клавишу...";
+
+            string userInputCommand;
+            bool isTired = false;
+
+            while (isTired == false)
+            {
+                Console.Clear();
+
+                Console.WriteLine(WelcomeMessage);
+                Console.WriteLine(MenuHeaderMessage);
+                Console.WriteLine($"{CommandGoToCafe} - пройти в кафе");
+                Console.WriteLine($"{CommandGoToCinema} - пройти в кинотеатр");
+                Console.WriteLine($"{CommandGoToBank} - пройти к банкомату");
+                Console.WriteLine($"{CommandGoToExit} - выйти");
+                Console.Write("\nВаша команда: ");
+
+                userInputCommand = Console.ReadLine().ToLower();
+
+                switch (userInputCommand)
+                {
+                    case CommandGoToExit:
+                        isTired = true;
+                        Console.WriteLine("Вы вышли из центра");
+                        break;
+
+                    case CommandGoToCafe:
+                        Console.WriteLine("Вы заказали кофе с круасанов в кафе");
+                        break;
+
+                    case CommandGoToCinema:
+                        Console.WriteLine("Вы купили билет на фильм: Дедпул и Росамаха");
+                        break;
+
+                    case CommandGoToBank:
+                        Console.WriteLine("Вы воспользовались банкоматом для снятия наличных");
+                        break;
+
+                    default:
+                        Console.WriteLine($"Такой команды - \"{userInputCommand}\" нет, попробуйте ввести другую");
+                        Console.ReadKey();
+                        continue;
+                }
+
+                Console.WriteLine(ContinueMessage);
+                Console.ReadKey();
+            }
         }
     }
 }
