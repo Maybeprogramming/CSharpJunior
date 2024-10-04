d1f1d9913d40f70f3defb5b0fd7bcb865a06e2ef
Maybeonce <a-u-rom@yandex.ru>
2024-10-04 10:39:50 +0300

    Решение задачи 17

--- 17_Task/Program.cs
+++ 17_Task/Program.cs
@@ -4,6 +4,25 @@
     {
         static void Main()
         {
+            Console.Title = "ДЗ: Степень двойки";
+            int randomNumber;
+            int maxRandomNumber = 1000;
+            int totalNumber;
+            int number = 2;
+            int degreeNumber = 1;
+            Random random = new Random();
+            randomNumber = random.Next(maxRandomNumber);
+            totalNumber = number;
+
+            while (randomNumber >= totalNumber)
+            {
+                totalNumber *= number;
+                degreeNumber++;
+            }
+
+            Console.WriteLine($"Число {number} в степени {degreeNumber} превосходит заданное число {randomNumber}");
+            Console.WriteLine($"Результат: {totalNumber} > {randomNumber}");
+            Console.ReadKey();
         }
     }
 }
