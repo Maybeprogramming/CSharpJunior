faa81612a8410266b98d15245551cb46a162b94d
Maybeonce <a-u-rom@yandex.ru>
2024-10-04 10:05:44 +0300

    Решение задачи 15

--- 15_Task/Program.cs
+++ 15_Task/Program.cs
@@ -4,32 +4,24 @@
     {
         static void Main()
         {
+            Console.Title = "ДЗ: Программа под паролем";
             string password = "qwerty";
             string userInput;
             int tryPasswordCount = 3;
             int leftTryInputPassword;
-            string welcomeMessage = $"Секретная база данных зоны 51";
-            string requestInputMessage = $"\nДля доступа к базе введите пароль: ";
-            string continueMessege = "\nНажмите клавишу чтобы продолжить...";
-            string topSecretFile = $"\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒" +
-                                    $"\n▒▒▄▀▀▀▀▀▄▒▒▒▒▒▄▄▄▄▄▒▒▒" +
-                                    $"\n▒▐░▄░░░▄░▌▒▒▄█▄█▄█▄█▄▒" +
-                                    $"\n▒▐░▀▀░▀▀░▌▒▒▒▒▒░░░▒▒▒▒" +
-                                    $"\n▒▒▀▄░═░▄▀▒▒▒▒▒▒░░░▒▒▒▒" +
-                                    $"\n▒▒▐░▀▄▀░▌▒▒▒▒▒▒░░░▒▒▒▒";
-            string topSecretMessege = "\nПришельцы существуют!!!";
-            string incorrectInputPassworMessage = $"\nВы неверно ввели пароль!";
+            string requestInputMessage = $"Введите пароль для доступа: ";
+            string continueMessege = "Нажмите клавишу чтобы продолжить";
+            string topSecretMessege = "Менторы, вы молодцы! Так держать!";
+            string incorrectInputPassworMessage = $"Пароль неверный!";
 
             for (int i = 0; i < tryPasswordCount; i++)
             {
                 Console.Clear();
-                Console.WriteLine(welcomeMessage);
                 Console.Write(requestInputMessage);
                 userInput = Console.ReadLine();
 
                 if (userInput == password)
                 {
-                    Console.WriteLine(topSecretFile);
                     Console.WriteLine(topSecretMessege);
                     Console.WriteLine(continueMessege);
                     Console.ReadLine();
@@ -38,14 +30,11 @@
                 {
                     leftTryInputPassword = tryPasswordCount - (i + 1);
                     Console.WriteLine(incorrectInputPassworMessage);
-                    Console.WriteLine($"У вас осталось {leftTryInputPassword} попыток.");
+                    Console.WriteLine($"Осталось {leftTryInputPassword} попыток для ввода пароля.");
                     Console.WriteLine(continueMessege);
                     Console.ReadLine();
                 }
             }
-
-            Console.WriteLine("\nРабота программы завершена.");
-            Console.ReadKey();
         }
     }
 }
