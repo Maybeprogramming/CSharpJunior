49f165e8b17323f08d46b93f992ad23461fd16c3
Maybeonce <a-u-rom@yandex.ru>
2024-10-04 10:23:46 +0300

    Решение задачи 16

--- 16_Task/Program.cs
+++ 16_Task/Program.cs
@@ -4,6 +4,28 @@
     {
         static void Main()
         {
+            Console.Title = "ДЗ: Кратные числа";
+            int number;
+            int startRandomNumber = 10;
+            int endRandomNumber = 25;
+            int minNumber = 50;
+            int maxlNumber = 150;
+            int amountNumbers = 0;
+            Random random = new Random();
+
+            number = random.Next(startRandomNumber, ++endRandomNumber);
+            Console.WriteLine($"Число N = {number}");
+
+            for (int i = number; i <= maxlNumber; i += number)
+            {
+                if (i >= minNumber)
+                {
+                    amountNumbers++;
+                }
+            }
+
+            Console.WriteLine($"Колличество кратных чисел: {amountNumbers}");
+            Console.ReadLine();
         }
     }
 }
