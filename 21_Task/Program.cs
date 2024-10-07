2fc85dac0d3a72bafe9a852c8d5d42fc6185815d
Maybeonce <a-u-rom@yandex.ru>
2024-10-07 11:17:11 +0300

    Решение задачи 21

--- 21_Task/Program.cs
+++ 21_Task/Program.cs
@@ -2,8 +2,54 @@
 {
     public class Program
     {
-        static void Main()
+        private static void Main()
         {
+            Console.Title = "ДЗ: Локальные максимумы";
+            int[] numbers;
+            int arraySize;
+            int firstNumberIndex = 0;
+            int lastNumberIndex;
+            int maxRandomNumber = 1000;
+            Random random = new Random();
+
+            Console.Write("Введите размер массива: ");
+            arraySize = Convert.ToInt32(Console.ReadLine());
+            numbers = new int[arraySize];
+            lastNumberIndex = arraySize - 1;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(maxRandomNumber);
+            }
+
+            Console.WriteLine($"Одномерный массив, размерностью {arraySize} элементов:");
+
+            foreach (int number in numbers)
+            {
+                Console.Write($"{number} ");
+            }
+
+            Console.WriteLine("\n\nЛокальные максимумы: ");
+
+            if (numbers[firstNumberIndex] > numbers[firstNumberIndex + 1])
+            {
+                Console.Write($"{numbers[firstNumberIndex]} ");
+            }
+
+            for (int i = firstNumberIndex + 1; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] > numbers[i - 1] && numbers[i] > numbers[i + 1])
+                {
+                    Console.Write($"{numbers[i]} ");
+                }
+            }
+
+            if (numbers[lastNumberIndex] > numbers[lastNumberIndex - 1])
+            {
+                Console.Write($"{numbers[lastNumberIndex]} ");
+            }
+
+            Console.ReadKey();
         }
     }
 }
