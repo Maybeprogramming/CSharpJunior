3a3263c55b0a983c199b6e95ee51d645be30bf4c
Maybeonce <a-u-rom@yandex.ru>
2024-10-07 14:00:52 +0300

    Решение задачи 25

--- 25_Task/Program.cs
+++ 25_Task/Program.cs
@@ -4,6 +4,26 @@
     {
         static void Main()
         {
+            Console.Title = "ДЗ: Split";
+            string sourceString = "Метод Split возвращает массив строк, " +
+                                  "содержащий подстроки в этом экземпляре, " +
+                                  "разделенные элементами указанной строки " +
+                                  "или массива символов Юникода.";
+
+            char spaceChar = ' ';
+
+            string[] wordsArray = sourceString.Split(spaceChar);
+            Console.WriteLine("Исходный текст:\n");
+            Console.WriteLine(sourceString);
+            Console.WriteLine();
+            Console.WriteLine("Результат метода String.Split():\n");
+
+            foreach (string word in wordsArray)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.ReadKey();
         }
     }
 }
