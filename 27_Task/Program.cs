cd7dd6fe8ecf5bbd84a340eff5d1d70524ed32b0
Maybeonce <a-u-rom@yandex.ru>
2024-10-07 14:47:21 +0300

    Решение задачи 27

--- 27_Task/Program.cs
+++ 27_Task/Program.cs
@@ -4,6 +4,49 @@
     {
         static void Main()
         {
+            Console.Title = "ДЗ: Скобочное выражение";
+            string userInput;
+            int bracketsCount = 0;
+            int depthCount = 0;
+            int balanceValue = 0;
+            char bracketOpenChar = '(';
+            char bracketCloseChar = ')';
+
+            Console.WriteLine("Введите строку из символов: \"(\" и \")\".");
+            userInput = Console.ReadLine();
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                if (userInput[i] == bracketOpenChar)
+                {
+                    bracketsCount++;
+                }
+                else if (userInput[i] == bracketCloseChar)
+                {
+                    if (i != userInput.Length - 1 && userInput[i + 1] != bracketOpenChar)
+                    {
+                        depthCount++;
+                    }
+
+                    bracketsCount--;
+                }
+
+                if (bracketsCount < balanceValue)
+                {
+                    break;
+                }
+            }
+
+            if (bracketsCount == balanceValue)
+            {
+                Console.WriteLine($"\"{userInput}\" - cтрока корректная, максимальная глубина составляет: {depthCount + 1}");
+            }
+            else
+            {
+                Console.WriteLine($"\"{userInput}\" - некорректная строка!");
+            }
+
+            Console.ReadKey();
         }
     }
 }
