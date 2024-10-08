c46034331cc32a887133c99940bf2d54b4342039
Maybeonce <a-u-rom@yandex.ru>
2024-10-08 14:55:41 +0300

    Задача 29

--- 29_Task/Program.cs
+++ 29_Task/Program.cs
@@ -4,38 +4,51 @@
     {
         private static void Main()
         {
+            Console.Title = "ДЗ: UIElement";
 
+            string healthBarName = "HP";
+            int healthPercentValue = 40;
+            int topPositionHealthBar = 1;
+            int leftPositionHealthBar = 5;
+            int lengthHealthBar = 20;
+
+            string manaBarName = "MP";
+            int manaPercentValue = 60;
+            int topPositionManaBar = 3;
+            int leftPositionManaBar = 5;
+            int lengthManaBar = 10;
+
+            DrawBar(healthBarName, healthPercentValue, topPositionHealthBar, leftPositionHealthBar, lengthHealthBar);
+            DrawBar(manaBarName, manaPercentValue, topPositionManaBar, leftPositionManaBar, lengthManaBar);
+
+            Console.ReadLine();
         }
 
-        private static void DrawBar(string nameBar, int value, int maxValue, int topPosition, int leftPosition, int barLength = 10, char valueSymbol = '#', char remainingSymbol = '_')
+        private static void DrawBar(string nameBar, int currentPercent, int topPosition = 0, int leftPosition = 0, int barLength = 10)
         {
             string bar = "";
-            int fullScalePercent = 100;
-            int maxDrawSymbols = 5;
-            int valueForDraw = value * fullScalePercent / maxValue / maxDrawSymbols;
-            float remainderOfDivision = ((float)value * fullScalePercent) / maxValue - (valueForDraw * maxDrawSymbols);
+            char valueChar = '#';
+            char emptyChar = '_';
+            int maxPercent = 100;
 
-            if (remainderOfDivision > 0)
-            {
-                valueForDraw++;
-            }
+            int valueForDraw = barLength * currentPercent / maxPercent;
 
             for (int i = 0; i < valueForDraw; i++)
             {
-                bar += valueSymbol;
+                bar += valueChar;
             }
 
             Console.SetCursorPosition(leftPosition, topPosition);
-            Console.Write($"[{bar}");
+            Console.Write($"{nameBar}: [{bar}");
 
             bar = "";
 
-            for (int i = valueForDraw; i < maxDrawSymbols; i++)
+            for (int i = valueForDraw; i < barLength; i++)
             {
-                bar += remainingSymbol;
+                bar += emptyChar;
             }
 
-            Console.Write($"{bar}] [{value}/{maxValue}] {nameBar}");
+            Console.Write($"{bar}] [{currentPercent}/{maxPercent}%]");
         }
     }
 }
