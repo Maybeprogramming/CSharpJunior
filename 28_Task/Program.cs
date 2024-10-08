0592db94f2326f80f11dcc84fc61317acb7dca68
Maybeonce <a-u-rom@yandex.ru>
2024-10-08 09:39:46 +0300

    Задача 28, добавление пользовательской библиотеки UserUtils

--- 28_Task/Program.cs
+++ 28_Task/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace _28_Task
+﻿using UserUtils;
+
+namespace _28_Task
 {
     public class Program
     {
-        static void Main()
+        private static void Main()
         {
             Console.Title = "ДЗ: ReadInt";
+
+            Work();
         }
+
+        private static void Work()
+        {
+            bool isTryParse = false;
+            string requestNumber = "Введите число: ";
+            string continueMessage = "\n\nНажмите любую клавишу чтобы продолжить...";
+            string userInput;
+            int number;
+
+            while (isTryParse == false)
+            {
+                Console.Clear();
+                Display.Print(requestNumber);
+                userInput = Console.ReadLine();
+                number = ParseStringToInt(userInput, out bool isParseToInt);
+
+                if (isParseToInt == true)
+                {
+                    Display.Print($"\nВы ввели число: {number}", ConsoleColor.Green);
+                    isTryParse = isParseToInt;
+                }
+                else
+                {
+                    Display.Print($"\nВы ввели не число: {userInput}", ConsoleColor.Red);
+                }
+
+                Display.Print(continueMessage);
+                Console.ReadLine();
+            }
+        }
+
+        private static int ParseStringToInt(string userInput, out bool isParseToInt)
+        {
+            isParseToInt = int.TryParse(userInput, out int result);
+            return result;
+        }        
     }
 }
---
+++ UserUtils/Display.cs
@@ -0,0 +1,13 @@
+﻿namespace UserUtils
+{
+    public static class Display
+    {
+        public static void Print(string text, ConsoleColor color = ConsoleColor.White)
+        {
+            ConsoleColor defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ForegroundColor = defaultColor;
+        }
+    }
+}
