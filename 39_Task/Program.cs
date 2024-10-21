5e7c656da6d13e0d6402ac53cecb619b0d37cc92
Maybeonce <a-u-rom@yandex.ru>
2024-10-21 15:34:58 +0300

    ДЗ: Работа со свойствами

--- 39_Task/Program.cs
+++ 39_Task/Program.cs
@@ -4,6 +4,39 @@
     {
         static void Main()
         {
+            Console.Title = "ДЗ: Работа со свойствами";
+            string markPlayer = "@";
+            int positionX = 5;
+            int pisitionY = 5;
+
+            Player player = new Player(markPlayer, positionX, pisitionY);
+            Renderer renderer = new();
+            renderer.Draw(player);
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Player
+    {
+        public Player(string markSymbol, int positionX, int positionY)
+        {
+            MarkSymbol = markSymbol;
+            PositionX = positionX;
+            PositionY = positionY;
+        }
+
+        public string MarkSymbol { get; private set; }
+        public int PositionX { get; private set; }
+        public int PositionY { get; private set; }
+    }
+
+    public class Renderer
+    {
+        public void Draw(Player player)
+        {
+            Console.SetCursorPosition(player.PositionX, player.PositionY);
+            Console.WriteLine(player.MarkSymbol);
         }
     }
 }
