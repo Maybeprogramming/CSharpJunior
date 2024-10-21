2824afbfb42b24df441fee453ed07f09cea96c60
Maybeonce <a-u-rom@yandex.ru>
2024-10-21 10:37:00 +0300

    ДЗ: Работа с классами

--- 38_Task/Program.cs
+++ 38_Task/Program.cs
@@ -2,8 +2,48 @@
 {
     public class Program
     {
-        static void Main()
+        private static void Main()
         {
+            Console.Title = "ДЗ: Работа с классами";
+            string name = "Maybeonce";
+            string classType = "Воин";
+            int health = 100;
+            int damage = 25;
+
+            Player player = new Player(name, classType, health, damage);
+            player.ShowInfo();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Player
+    {
+        private string _name;
+        private string _classType;
+        private int _health;
+        private int _damage;
+
+        public Player(string name, string classType, int health, int damage)
+        {
+            _name = name;
+            _classType = classType;
+            _health = health;
+            _damage = damage;
+        }
+
+        public string Name => _name;
+        public string Class => _classType;
+        public int Health => _health;
+        public int Damage => _damage;
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"" +
+                $"Персонаж: {_name}" +
+                $"\nКласс: {_classType}" +
+                $"\nЗдоровье: [{_health}]" +
+                $"\nНаносимый урон: [{_damage}]");
         }
     }
 }
