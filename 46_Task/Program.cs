8bd5280c347d3a08a827ddfc65702b6ec1b7b84b
Maybeonce-HomePc <a-u-rom@yandex.ru>
2025-04-13 18:58:03 +0400

    Update Program.cs

--- 46_Task/Program.cs
+++ 46_Task/Program.cs
@@ -17,24 +17,159 @@
     {
         private int _moneyBalance;
         private Queue<Client> _clients;
+        private List<Product> _products;
 
         public Shop()
         {
             _moneyBalance = 0;
-            ClientFactory clientFactory = new ClientFactory();
-            _clients = clientFactory.FillClients();
+
+            ClientFactory clientFactory = new();
+            _clients = clientFactory.GetRandomClients();
+
+            ProductFactory productFactory = new();
+            _products = productFactory.GetProducts();
         }
 
         public void Work()
         {
+            const int ShopOpenCommand = 1;
+            const int ShowClientsQueueCommand = 2;
+            const int ShowProductsCommand = 3;
+            const int ExitCommand = 4;
+
+            int userInput;
+            bool isWork = true;
+
+            while (isWork)
+            {
+                Console.Clear();
+                UserUtils.Print($"Баланс магазина: [{_moneyBalance}] $\n\n", ConsoleColor.DarkYellow);
+
+                UserUtils.Print($"Команды:" +
+                    $"\n{ShopOpenCommand} - Открыть магазин" +
+                    $"\n{ShowClientsQueueCommand} - Посмотреть очередь покупателей" +
+                    $"\n{ShowProductsCommand} - Посмотреть ассортимент продуктов" +
+                    $"\n{ExitCommand} - Выйти из магазина");
+
+                UserUtils.Print($"\n\nВведите команду: ", ConsoleColor.Green);
+
+                switch (UserUtils.ReadInputNumber())
+                {
+                    case ShopOpenCommand:
+                        UserUtils.Print($"Тут будет открытие магазина", ConsoleColor.Green);
+                        break;
+                    case ShowClientsQueueCommand:
+                        ShowClientQueue();
+                        break;
+                    case ShowProductsCommand:
+                        ShowProducts();
+                        break;
+                    case ExitCommand:
+                        isWork = false;
+                        break;
+                    default:
+                        UserUtils.Print($"Нет такой команды! Попробуйте снова", ConsoleColor.Red);
+                        break;
+                }
+
+                Console.ReadLine();
+            }
+
+            //----------------------------- For Test
+            //ShowProducts();
+
+            //Console.WriteLine();
+
+            //ShowClientQueue();
+            //----------------------------- End Test
+        }
+
+        private void ShowProducts()
+        {
+            int index = 0;
+
+            UserUtils.Print($"\nАссортимент товаров:", ConsoleColor.Green);
+
+            foreach (Product product in _products)
+            {
+                UserUtils.Print($"\n{++index}. {product.GetInfo()}");
+            }
+        }
+
+        private void ShowClientQueue()
+        {
+            int index = 0;
+
+            UserUtils.Print($"\nОчередь покупателей:", ConsoleColor.Green);
+
+            foreach (Client client in _clients)
+            {
+                UserUtils.Print($"\n{++index}. {client.GetInfo()}");
+            }
         }
     }
 
     public class ClientFactory
     {
-        public Queue<Client>? FillClients()
+        public Queue<Client> GetRandomClients()
         {
-            throw new NotImplementedException();
+            int minClientCount = 3;
+            int maxClientCount = 9;
+            int clientCount = UserUtils.GenerateRandomNumber(minClientCount, ++maxClientCount);
+
+            Queue<Client> clients = new();
+
+            for (int i = 0; i < clientCount; i++)
+            {
+                clients.Enqueue(CreateClient());
+            }
+
+            return clients;
+        }
+
+        private Client CreateClient()
+        {
+            return new Client(GetRandomName());
+        }
+
+        private string GetRandomName()
+        {
+            List<string> names = new()
+            {
+                "Василий","Анна","Петр","Оля","Иван","Марина","Николай","Снежана","Алексей","Катя","Гена","Света",
+                "Данил","Василиса","Арсений","Карина", "Дима", "Галина","Максим", "Наташа", "Олег", "Дарья"
+            };
+
+            return names[UserUtils.GenerateRandomNumber(0, names.Count)];
+        }
+    }
+
+    public class ProductFactory
+    {
+        public List<Product> GetProducts()
+        {
+            List<string> productName = new()
+            {
+                "Печенье","Лимонад","Апельсины","Клубника","Огурцы","Помидоры",
+                "Хлеб","Колбаса","Сахар","Кофе","Шоколад","Пельмени","Котлеты","Арбуз"
+            };
+
+            List<Product> products = new();
+
+            for (int i = 0; i < productName.Count; i++)
+            {
+                products.Add(CreateProduct(productName[i]));
+            } 
+
+            return products;
+        }
+
+        private Product CreateProduct(string name)
+        {
+            int minPrice = 50;
+            int maxPrice = 150;
+
+            return new Product(name, UserUtils.GenerateRandomNumber(minPrice, maxPrice));
         }
     }
 
@@ -51,7 +186,7 @@
 
         public string GetInfo()
         {
-            return $"<{Name}> - цена: [{Price}]";
+            return $"<{Name}> - цена: [{Price}] $";
         }
     }
 
@@ -66,12 +201,15 @@
         private Bag _bag;
         private Bag _cart;
 
-        public Client()
+        public Client(string name)
         {
-            _wallet = new Wallet(1000);
+            Name = name;
+            _wallet = new Wallet(UserUtils.GenerateRandomNumber(300, 701));
             _bag = new Bag("Сумка");
             _cart = new Bag("Корзина");
         }
+
+        public string Name { get; }
 
         public bool TryBuyProducts()
         {
@@ -90,6 +228,11 @@
             {
                 return false;
             }
+        }
+
+        public string GetInfo()
+        {
+            return $"<{Name}>, у меня есть наличные: {_wallet.Money} $";
         }
     }
 
@@ -164,14 +307,7 @@
         private static Random s_random = new Random();
 
         public static int GenerateRandomNumber(int minNumber, int maxNumber) =>
-            s_random.Next(minNumber, ++maxNumber);
-
-        public static bool IsPositiveChance(int currentChancePercent, int minChancePercent = 0, int maxChancePercent = 100)
-        {
-            int randomNumber = GenerateRandomNumber(minChancePercent, maxChancePercent);
-
-            return randomNumber <= currentChancePercent;
-        }
+            s_random.Next(minNumber, maxNumber);
 
         public static void Print<T>(T message) =>
             Console.Write(message.ToString());
