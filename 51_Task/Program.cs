aabe835f195fb171d27e4071cd2f77c9962cb3cb
Maybeonce-HomePc <a-u-rom@yandex.ru>
2025-04-20 10:17:37 +0400

    Update Program.cs

--- 51_Task/Program.cs
+++ 51_Task/Program.cs
@@ -1,33 +1,179 @@
-﻿namespace _51_Task
+﻿using System.Drawing;
+
+namespace _51_Task
 {
     public class Program
     {
         static void Main()
         {
+            List<Criminal> criminals = new CriminalFactory().GetCriminals(30);
+
+            int index = 0;
+
+            UserUtils.Print($"Список преступников:");
+
+            foreach (var c in criminals)
+            {
+                UserUtils.Print($"\n{++index}. ");
+                c.ShowInfo();
+            }
+
+            Console.ReadKey();
         }
     }
 
     public class Criminal
     {
+        public Criminal(string name, string nationality, int height, int weight, bool isUnderArrest)
+        {
+            Name = name;
+            Nationality = nationality;
+            Height = height;
+            Weight = weight;
+            IsUnderArrest = isUnderArrest;
+        }
+
         public string Name { get; }
         public string Nationality { get; }
         public int Height { get; }
         public int Weight { get; }
         bool IsUnderArrest { get; }
+        string ArrestStatus => IsUnderArrest == true ? "под стражей" : "на свободе";
+
+        public string GetInfo() =>
+            $"{Name}, - {Nationality}, - Рост {Height}, - Вес {Weight}, - Статус <{ArrestStatus}>";
+
+        public void ShowInfo()
+        {
+            UserUtils.Print($"{Name}, Национальность <{Nationality}>, Рост <{Height}>, Вес <{Weight}>, Статус: ");
+            UserUtils.Print($"<{ArrestStatus}>", IsUnderArrest == true ? ConsoleColor.Red : ConsoleColor.Green);
+        }
     }
 
-    public class CrimanalFactory
-    { 
-
-    }
-
-    public static class CrimanalData
+    public class CriminalFactory
     {
-        private static string s_names;
-        private static string s_surNames;
-        private static int s_height;
-        private static int s_weight;
+        public List<Criminal> GetCriminals(int count)
+        {
+            List<Criminal> criminals = new();
 
+            for (int i = 0; i < count; i++)
+            {
+                criminals.Add(CreateCrimanal());
+            }
 
+            return criminals;
+        }
+
+        private Criminal CreateCrimanal()
+        {
+            string name = CriminalData.GetRandomName() + " " + CriminalData.GetRanddomSurName();
+            string nationality = CriminalData.GetRandomNationality();
+            int height = CriminalData.GetRandomHeight();
+            int weight = CriminalData.GetRandomWeight();
+            bool isUnderArrest = IsUnderArrestRandom();
+
+            return new Criminal(name, nationality, height, weight, isUnderArrest);
+        }
+
+        private bool IsUnderArrestRandom()
+        {
+            bool[] statusArrests = new[]
+            {
+                true, false
+            };
+
+            return statusArrests[UserUtils.GenerateRandomNumber(0, statusArrests.Length - 1)];
+        }
+    }
+
+    public static class CriminalData
+    {
+        private static string[] s_names;
+        private static string[] s_surNames;
+        private static int[] s_height;
+        private static int[] s_weight;
+        private static string[] s_nationality;
+
+        static CriminalData()
+        {
+            s_names = new string[]
+            {
+                "Павел", "Иван", "Сергей", "Олег", "Константин",
+                "Анатолий", "Аркадий", "Петр", "Вячеслав", "Николай",
+                "Владислав", "Роман", "Дмитрий", "Василий", "Михаил",
+                "Руслан", "Равиль", "Фёдор", "Валерий", "Евгений"
+            };
+
+            s_surNames = new string[]
+            {
+                "Иванов", "Петров", "Сидоров", "Овечкин", "Царьков",
+                "Овсянников", "Пирожков", "Галкин", "Пугачев", "Ивушкин",
+                "Настольников", "Дубровский", "Ушкин", "Васильев", "Тарзанов",
+                "Авушкин", "Фролов", "Никитин", "Маркин", "Сливушкин"
+            };
+
+            s_nationality = new string[]
+            {
+                "Русский", "Татарин", "Чувашин", "Башкир", "Мордвин"
+            };
+
+            s_height = new int[]
+            {
+                150, 160, 170, 180, 190
+            };
+
+            s_weight = new int[]
+            {
+                60, 70, 80, 90, 100
+            };
+        }
+
+        public static string GetRandomName() =>
+            GetRandomElement(s_names);
+
+        public static string GetRanddomSurName() =>
+            GetRandomElement(s_surNames);
+
+        public static int GetRandomHeight() =>
+            GetRandomElement(s_height);
+
+        public static int GetRandomWeight() =>
+            GetRandomElement(s_weight);
+
+        public static string GetRandomNationality() =>
+           GetRandomElement(s_nationality);
+
+        private static T GetRandomElement<T>(T[] array) =>
+            array[UserUtils.GenerateRandomNumber(0, array.Length - 1)];
+    }
+
+    public static class UserUtils
+    {
+        private static Random s_random = new Random();
+
+        public static int GenerateRandomNumber(int minNumber, int maxNumber) =>
+            s_random.Next(minNumber, ++maxNumber);
+
+        public static void Print<T>(T message) =>
+            Console.Write(message.ToString());
+
+        public static void Print<T>(T message, ConsoleColor consoleColor = ConsoleColor.White)
+        {
+            Console.ForegroundColor = consoleColor;
+            Print(message);
+            Console.ResetColor();
+        }
+
+        public static int ReadInputNumber()
+        {
+            int result;
+
+            while (int.TryParse(Console.ReadLine(), out result) == false)
+            {
+                Print($"\nВы ввели не число!\nПопробуйте снова: ", ConsoleColor.DarkYellow);
+            }
+
+            return result;
+        }
     }
 }
